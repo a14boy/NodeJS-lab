6ebe0423913837288804fe5a92cb6ca8da0f2bcf
Emuefujii Esusanbyakugojyuni (MFG S352) <030300AR@jp.screen-net.local>
2022-04-25 17:09:03 +0900

    lab01-6

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ const fs = require("fs");
 const sever = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  console.log(req.url, req.method, req.header);
-  if (req.url === "/") {
-    res.setHeader("content-type", "text");
+  if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message!</title></head>");
     res.write(
-      "<body><form action ='/message' method='POST'><input type= 'text'><button type='submit'>Send</button></form></body>"
+      "<body><form action ='/message' method='POST'><input type= 'text' name='message'><button type='submit'>Send</button></form></body>"
     );
     res.write("</html>");
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(parsedBody);
+      fs.writeFileSync("message.txt", message);
+    });
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
   }
-  res.setHeader("content-type", "text");
+  res.setHeader("content-type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page!</title></head>");
   res.write("<body><h1>Hello from my Node.js server!</h1></body>");
