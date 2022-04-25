0c229c521d2045639ac8fa224be2424d48a73e6e
Emuefujii Esusanbyakugojyuni (MFG S352) <030300AR@jp.screen-net.local>
2022-04-25 14:28:49 +0900

    lab01-4

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require("http");
 const sever = http.createServer((req, res) => {
   console.log(req.url, req.method, req.header);
+  if (req.url === "/") {
+    res.setHeader("content-type", "text");
+    res.write("<html>");
+    res.write("<head><title>Enter Message!</title></head>");
+    res.write(
+      "<body><form action ='/message' method='POST'><input type= 'text'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("content-type", "text");
   res.write("<html>");
   res.write("<head><title>My first page!</title></head>");
