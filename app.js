3bd32c8cb0c49f922b18c3c575662cb3a5dea1a6
Emuefujii Esusanbyakugojyuni (MFG S352) <030300AR@jp.screen-net.local>
2022-04-27 17:18:18 +0900

    lab02-2

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("First MiddleWare!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Second MiddleWare!");
+  next();
+});
+
 const sever = http.createServer(app);
 
 sever.listen(3000);
