190e3cd4a0d616eef6385a2ae9bf0565c08c2639
Emuefujii Esusanbyakugojyuni (MFG S352) <030300AR@jp.screen-net.local>
2022-04-28 17:33:17 +0900

    lab02-5

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("First MiddleWare!");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  res.send("<html><h1>The Add product page</h1></html>");
+  res.send(
+    "<form action='/product' method ='POST'><input type='text' name='title'><button type='submit'>add product</button></form>"
+  );
 });
-app.use((req, res, next) => {
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<html><h1>Hello from Express.js</h1></html>");
 });
 
